4fc4d5014d301b11726879ef5553d7d00e7911cb
DavideCiammetti <davideciammetti97@gmail.com>
2023-12-04 12:01:39 +0100

    feat: funzione elimina messaggi

--- milestone 5/main.js
+++ milestone 5/main.js
@@ -18,19 +18,22 @@ createApp({
                         date: '10/01/2020 15:30:55',
                         message: 'Hai portato a spasso il cane?',
                         status: 'sent',
-                        dropDown: false
+                        dropDown: false,
+                        indexMenu: false,
                     },
                     {
                         date: '10/01/2020 15:50:00',
                         message: 'Ricordati di stendere i panni',
                         status: 'sent',
-                        dropDown: false
+                        dropDown: false,
+                        indexMenu: false,
                     },
                     {
                         date: '10/01/2020 16:15:22',
                         message: 'Tutto fatto!',
                         status: 'received',
-                        dropDown: false
+                        dropDown: false,
+                        indexMenu: false,
                     }
                 ],
             },
@@ -43,19 +46,22 @@ createApp({
                         date: '20/03/2020 16:30:00',
                         message: 'Ciao come stai?',
                         status: 'sent',
-                        dropDown: false
+                        dropDown: false,
+                        indexMenu: false,
                     },
                     {
                         date: '20/03/2020 16:30:55',
                         message: 'Bene grazie! Stasera ci vediamo?',
                         status: 'received',
-                        dropDown: false
+                        dropDown: false,
+                        indexMenu: false,
                     },
                     {
                         date: '20/03/2020 16:35:00',
                         message: 'Mi piacerebbe ma devo andare a fare la spesa.',
                         status: 'sent',
-                        dropDown: false
+                        dropDown: false,
+                        indexMenu: false,
                     }
                 ],
             },
@@ -68,19 +74,22 @@ createApp({
                         date: '28/03/2020 10:10:40',
                         message: 'La Marianna va in campagna',
                         status: 'received',
-                        dropDown: false
+                        dropDown: false,
+                        indexMenu: false,
                     },
                     {
                         date: '28/03/2020 10:20:10',
                         message: 'Sicuro di non aver sbagliato chat?',
                         status: 'sent',
-                        dropDown: false
+                        dropDown: false,
+                        indexMenu: false,
                     },
                     {
                         date: '28/03/2020 16:15:22',
                         message: 'Ah scusa!',
                         status: 'received',
-                        dropDown: false
+                        dropDown: false,
+                        indexMenu: false,
                     }
                 ],
             },
@@ -93,13 +102,15 @@ createApp({
                         date: '10/01/2020 15:30:55',
                         message: 'Lo sai che ha aperto una nuova pizzeria?',
                         status: 'sent',
-                        dropDown: false
+                        dropDown: false,
+                        indexMenu: false,
                     },
                     {
                         date: '10/01/2020 15:50:00',
                         message: 'Si, ma preferirei andare al cinema',
                         status: 'received',
-                        dropDown: false
+                        dropDown: false,
+                        indexMenu: false,
                     }
                 ],
             },
@@ -112,13 +123,15 @@ createApp({
                         date: '10/01/2020 15:30:55',
                         message: 'Ricordati di chiamare la nonna',
                         status: 'sent',
-                        dropDown: false
+                        dropDown: false,
+                        indexMenu: false,
                     },
                     {
                         date: '10/01/2020 15:50:00',
                         message: 'Va bene, stasera la sento',
                         status: 'received',
-                        dropDown: false
+                        dropDown: false,
+                        indexMenu: false,
                     }
                 ],
             },
@@ -131,19 +144,22 @@ createApp({
                         date: '10/01/2020 15:30:55',
                         message: 'Ciao Claudia, hai novità?',
                         status: 'sent',
-                        dropDown: false
+                        dropDown: false,
+                        indexMenu: false,
                     },
                     {
                         date: '10/01/2020 15:50:00',
                         message: 'Non ancora',
                         status: 'received',
-                        dropDown: false
+                        dropDown: false,
+                        indexMenu: false,
                     },
                     {
                         date: '10/01/2020 15:51:00',
                         message: 'Nessuna nuova, buona nuova',
                         status: 'sent',
-                        dropDown: false
+                        dropDown: false,
+                        indexMenu: false,
                     }
                 ],
             },
@@ -156,13 +172,15 @@ createApp({
                         date: '10/01/2020 15:30:55',
                         message: 'Fai gli auguri a Martina che è il suo compleanno!',
                         status: 'sent',
-                        dropDown: false
+                        dropDown: false,
+                        indexMenu: false,
                     },
                     {
                         date: '10/01/2020 15:50:00',
                         message: 'Grazie per avermelo ricordato, le scrivo subito!',
                         status: 'received',
-                        dropDown: false
+                        dropDown: false,
+                        indexMenu: false,
                     }
                 ],
             },
@@ -175,19 +193,22 @@ createApp({
                         date: '10/01/2020 15:30:55',
                         message: 'Ciao, andiamo a mangiare la pizza stasera?',
                         status: 'received',
-                        dropDown: false
+                        dropDown: false,
+                        indexMenu: false,
                     },
                     {
                         date: '10/01/2020 15:50:00',
                         message: 'No, l\'ho già mangiata ieri, ordiniamo sushi!',
                         status: 'sent',
-                        dropDown: false
+                        dropDown: false,
+                        indexMenu: false,
                     },
                     {
                         date: '10/01/2020 15:51:00',
                         message: 'OK!!',
                         status: 'received',
-                        dropDown: false
+                        dropDown: false,
+                        indexMenu: false,
                     }
                 ],
             }
@@ -195,7 +216,6 @@ createApp({
         curIndex: 0,
         newMessage: '',
         searchText: '',
-        indexMenu: true,
     };
   },
 
@@ -213,7 +233,8 @@ createApp({
                     date: 0,
                     message: this.newMessage,
                     status: 'sent',
-                    dropDown: false
+                    dropDown: false,
+                    indexMenu: false,
                 });
                 this.timeResponse();
                 this.newMessage = '';
@@ -230,7 +251,8 @@ createApp({
                 date: 0,
                 message: 'ok',
                 status: 'received',
-                dropDown: false
+                dropDown: false,
+                indexMenu: false,
             });
 
         },
@@ -270,6 +292,12 @@ createApp({
                   message[mexIndex].dropDown = false; 
                 }
                 console.log(message.dropDown);    
+          },
+        //   funzione elimin messaggi---> ho messo un v-show sul secondo div con classe="two-message-container" che al click imposta display none alla riga 
+          deleteMessage(mexIndex,index){
+            const contact = this.contacts[index];
+            let message = contact.messages;
+            message[mexIndex].indexMenu = true;
           },
           
           
